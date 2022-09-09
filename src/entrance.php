6f75254b0eea0c8d9a59c9761369dd36679f7e17
chenmingsheng <1159604116@qq.com>
2022-09-09 16:59:26 +0800

    提交

---
+++ src/entrance.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by composer.baigebaodev1.com.
+ * User: cms
+ * Date: 2022/9/8
+ * Time: 15:17
+ */
+namespace tool\src;
+class entrance{
+    public function __construct()
+    {
+
+    }
+
+    public function demo(){
+        echo "hello jack";
+    }
+}
